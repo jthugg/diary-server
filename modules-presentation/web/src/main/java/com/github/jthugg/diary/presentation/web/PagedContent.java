d66b589a130b3c934ca8462a93276964db216233
Hi. I'm Neo <jthug@naver.com>
2024-06-29 15:44:51 +0900

    fix: 프레젠테이션 레이어 공통 모델 수정
    미사용 어노테이션 제거

--- modules-presentation/web/src/main/java/com/github/jthugg/diary/presentation/web/PagedContent.java
+++ modules-presentation/web/src/main/java/com/github/jthugg/diary/presentation/web/PagedContent.java
@@ -1,11 +1,9 @@
 package com.github.jthugg.diary.presentation.web;
 
 import lombok.Builder;
-import lombok.Getter;
 
 import java.util.List;
 
-@Getter
 @Builder
 public record PagedContent<T>(
         List<T> contents,
