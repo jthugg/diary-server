c16b0b723c449009f8c3685de2a20d6cb8638d7a
Hi. I'm Neo <jthug@naver.com>
2024-06-30 00:20:50 +0900

    fix: Ping 컨트롤러 엔드포인트 추가

--- modules-gateway/ping/src/main/java/com/github/jthugg/diary/dateway/ping/PingController.java
+++ modules-gateway/ping/src/main/java/com/github/jthugg/diary/dateway/ping/PingController.java
@@ -1,6 +1,7 @@
 package com.github.jthugg.diary.dateway.ping;
 
 import com.github.jthugg.diary.presentation.web.Response;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +18,11 @@ public class PingController {
     @GetMapping("/check")
     public HttpEntity<Response<String>> check() {
         return Response.ofEntity(HttpStatus.OK, "check");
+    }
+
+    @GetMapping({"/public", "/anonymous", "/authenticated"})
+    public HttpEntity<Response<String>> publicMethod(HttpServletRequest request) {
+        return Response.ofEntity(HttpStatus.OK, request.getRequestURI());
     }
 
 }
